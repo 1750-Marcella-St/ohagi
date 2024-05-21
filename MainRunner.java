3460cf99c111a407409090ab4c81968c3e3d9de6
1750-Marcella-St <toxicallogy@gmail.com>
2024-05-21 09:32:30 -0700

    make MainRunner

--- MainRunner.java
+++ MainRunner.java
@@ -1,5 +1,20 @@
 public class MainRunner {
     public static void main(String[] args) {
-        //code
+        String word = "ginger";
+        printAnagrams(word, "");
+    }
+    public static void printAnagrams(String word, String anagram) {
+        if (word.length() == 0) {
+            System.out.println(anagram);
+            return;
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            String left = word.substring(0, i);
+            String right = word.substring(i + 1);
+            String rest = left + right;
+            printAnagrams(rest, anagram + c);
+        }
     }
 }
